da4be2c430351c8a882a945e6fd48807f4992c83
Kevin Membreno <83984148+JustYourFriendlyNeighborhoodProgrammer@users.noreply.github.com>
2022-09-16 16:43:32 -0500

    Update Assignment_1.cpp

--- Assignment_1.cpp
+++ Assignment_1.cpp
@@ -1,19 +1,6 @@
 /*
-Write a C++ program that can read data (let’s say file that has 10 lines,
- and each line contains 10 integers) from a file into an array and
- perform the following functions:
-
-1. Check if a certain integer exists in the array if the number is present return 
- the index where the number is present. 
-
-2. A function that can modify the value of an integer when called with the index
- of the integer in the array and return the new value and old value back to the user.
-
-3. A function that adds a new integer to the end of the array.
-
-4. A function which intakes an index of an array and remove
- the integer corresponding to that index.
-
+    Author: Kevin Membreno
+    Date:   9/26/2022
 */
 #include <iostream>
 #include <fstream>
@@ -134,22 +121,3 @@ void header:: removeInt (int index) {
     array_size--; // decrements my_array size 
 
 }
-
-/*
-Submission guidelines:
-
-1)	You should have a header file and .cpp file.
- The header file should provide the function declaration
- and .cpp file should have implementation details.
-
-2)	All the functionality of the program should be implemented as functions and methods.
-
-3)	The code should be well commented.
-
-4)	Create a report (readme file) that contains 
- instruction on how to run the code and screen shots of the outputs
-
-5)	Upload your report and code files to GitHub.
-
-6)	Submit the GitHub link on Canvas by due date. 
-*/
