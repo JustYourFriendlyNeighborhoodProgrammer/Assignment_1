da97cdbac6a86b85d0b5ffbadc016588574c4d18
Kevin Membreno <83984148+JustYourFriendlyNeighborhoodProgrammer@users.noreply.github.com>
2022-11-28 16:34:41 -0600

    Add files via upload

---
+++ Part 2/Assignment_3.cpp
@@ -0,0 +1,53 @@
+/*
+As discussed in chapter 5, a palindrome is a word that reads the same left to right as 
+ right to left. Write a recursive function that determines whether its argument string is a 
+ palindrome.
+
+test for palindrome:
+kayak
+i saw i was i
+able was i ere i saw elba
+level madam level
+noon
+anna
+
+*/
+
+
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+bool palindrome(string word, int i, int j);
+
+int main() {
+    char str[100];
+    string temp;
+    printf("Enter a string to see whether or not it is a palindrome. \n");
+    scanf("%[^\n]s", str);
+    temp = str;
+    
+    if (palindrome(temp, 0, temp.length() - 1)) {
+        printf("The string is a palindrome!");
+    } else {
+        printf("the string is not a palindrome.");
+    }
+    
+    printf("\n");
+    system("pause");
+    return 0;
+}
+
+bool palindrome(string word, int i, int j)
+{
+    if (word.at(i++) == word.at(j--)) {
+        if (j > i) {
+            return palindrome(word, i, j);
+        }
+        return true;
+    } else {
+        return false;
+    }   
+}
---
+++ Part 5/Assignment_3.cpp
@@ -0,0 +1,60 @@
+/*
+Declare the map container stateDataMap to store pairs of the form (stateName,capitalName),
+ where stateName and capitalName are variables of type string:
+ 
+1.  write c++ statements that add the following pairs to stateDataMap:
+ (Nebraska, Lincoln), (New York, Albany), (Ohio, Columbus), (California, Sacramento),
+ (Massachusetts, Boston), and (Texas, Austin).
+
+2.  write a C++ method that outputs the data stored in stateDateMap.
+3.  write a C++ statement that changes the capital of California to Los Angeles.
+4.  write a C++ method that outputs the capitalName using the stateName which will
+    be entered by the user.
+
+*/
+
+
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <iterator>
+
+using namespace std;
+
+int main() {
+    string temp;
+    // map container stateDataMap that stores pairs of the from (stateName, capitalName)
+    map<string,string> stateDataMap;
+
+    // statements that add the following pairs to stateDataMap
+    stateDataMap["Nebraska"] = "Lincoln";
+    stateDataMap["New York"] = "Albany";
+    stateDataMap["Ohio"] = "Columbus";
+    stateDataMap["California"] = "Sacramento";
+    stateDataMap["Massachusetts"] = "Boston";
+    stateDataMap["Texas"] = "Austin";
+
+    // a method that outputs the data stored in stateDataMap
+    for (auto& itr : stateDataMap) {
+        cout << itr.first << " ";
+        cout << itr.second << "\n";
+    }
+    
+    // statement that changes the capital of California to Los Angeles
+    stateDataMap["California"] = "Los Angeles";
+
+    cout << "\n";
+    
+    // a method that outputs the capitalName using the stateName
+    cout << "Enter a state name to find out its capital: ";
+    cin >> temp;
+    for (auto& itr : stateDataMap) {
+        if (itr.first == temp)
+        {
+            cout << "It's capital is: " << itr.second << "\n";
+        }
+    }
+    return 0;
+}
+
